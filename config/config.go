c938e72db29568330a6b8937d9ea33930a2b6cf0
Ian <ianmdawson@gmail.com>
2016-10-22 00:54:13 -0700

    initial server implementation

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"os"
+
+	"github.com/subosito/gotenv"
+)
+
+// LoadEnv loads a json configuration file
+func LoadEnv(path string) error {
+	if path == "" {
+		path = "../.env"
+	}
+
+	return gotenv.Load(path, "credentials")
+}
+
+// IsProductionEnvironment returns true if environment is production
+func IsProductionEnvironment() bool {
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "production" {
+		return true
+	}
+	return false
+}
--- server.go
+++ server.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ianmdawson/go_server/config"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", "Title", "Body")
+}
+
 func main() {
-	return
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	if !config.IsProductionEnvironment() {
+		config.LoadEnv("")
+	}
+
+	// http.HandleFunc("/api/comments", viewHandler)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	fmt.Printf("Getting ready to serve on port: %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
